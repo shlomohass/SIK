41fcdc5a37905b3b7fe36443e4d971d2335cecde
shlomohassid@gmail.com <shlomohassid@gmail.com>
2016-11-06 00:15:21 +0200

    Initial Commit

---
+++ SIK/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SIK
+//
+//  Created by Shlomo Hassid on 06/11/2016.
+//  Copyright © 2016 Shlomo Hassid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
